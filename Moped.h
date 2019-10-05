392525f52f3249be5b66becaa9974d90ccc0f4fd
VanderMadison <mlvander0318@gmail.com>
2019-10-04 20:59:33 -0400

    Declared functions for Moped

---
+++ Moped.h
@@ -0,0 +1,27 @@
+/*
+ *Madison Vandersluis
+ */
+
+#ifndef DRIVINGSIMULATOR_MOPED_H
+#define DRIVINGSIMULATOR_MOPED_H
+
+#include "PoweredVehicle.h"
+
+class Moped : public PoweredVehicle {
+	private:
+		int ccEngineSize;
+
+	public:
+		Moped();
+
+		explicit Moped(string brand, string model, 
+				stringFuelType, int engineSize);
+
+		virtual ~Moped();
+		string getEngineSize();
+		void setEngineSize(int engineSize);
+		virtual double mileageEstimate(double time);
+		virtual string toString();
+};
+
+#endif
